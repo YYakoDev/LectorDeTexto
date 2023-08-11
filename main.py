2985993d611626f67ea5e8c669d3a1f077ade22e
YYakoDev <carrascojoaquin10@gmail.com>
2023-08-11 17:58:26 -0300

    solucionado el impedimento de poder copiar el texto en una aplicacion que tuviera el f3 asignado a otra funcion

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import time
 import keyboard
 from clipboard_manager import ClipboardGetter
-from tts_manager import readText, check_file_exists, filePath
+from tts_manager import readText
 
 program_running = True
 readHotkey = "f3"
@@ -15,11 +15,10 @@ print("Utilice f3 para que el programa lea el texto que tengas copiado \n"
       "para salir del programa presione f10")
 
 
-keyboard.add_hotkey(readHotkey, lambda: read_hotkey_action())
+keyboard.add_hotkey(readHotkey, lambda: read_hotkey_action(), suppress=True)
 
 
 def read_hotkey_action():
-    #if(check_file_exists(filePath)):
     # here you need to check if the program is already reading and stop that
     keyboard.press("ctrl")
     keyboard.press_and_release("c")
